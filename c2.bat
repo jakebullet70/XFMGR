5cfdff0d8d154bc53eececae2bfea42e2c7d113a
Steven De George SR <ehomecreations@gmail.com>
2026-01-01 10:41:17 +0200

    refactor

--- c2.bat
+++ c2.bat
@@ -1,4 +1,4 @@
 rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 hd.p8
-java.exe -jar C:\8bitProgramming\prog8\prog8c-12.0.1-all.jar -target cx16 test1.p8
+java.exe -jar C:\8bitProgramming\prog8\prog8c-12.0.1-all.jar -target cx16 test2.p8
 pause
-x16noCard1x.bat
+rem x16noCard1x.bat
