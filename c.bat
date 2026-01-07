51167bbe5ddf979ac85a5dbcc6581a2136739497
Steven De George SR <ehomecreations@gmail.com>
2026-01-07 22:09:01 +0200

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,5 +1,5 @@
-rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 hd.p8
 echo COMPILED CX16 STREE
+rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 stree.p8
 java.exe -jar C:\8bitProgramming\prog8\prog8c-12.0.1-all.jar -target cx16 stree.p8
 pause
 rem x16noCard1x.bat
