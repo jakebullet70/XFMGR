af63021b0e6e28704768361853e7a8755ece523f
Steven De George SR <ehomecreations@gmail.com>
2026-01-22 07:43:16 +0200

    Changed compiler target

--- src/c.bat
+++ src/c.bat
@@ -2,7 +2,7 @@ echo COMPILED CX16 STREE
 rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 stree.p8
 del stree.prg
 del ..\stree.prg
-java.exe -jar C:\8bitProgramming\prog8\prog8c-12.0.1-all.jar -target cx16 stree.p8
+java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
 rem pause
 copy stree.prg ..\stree.prg
 del stree.asm
