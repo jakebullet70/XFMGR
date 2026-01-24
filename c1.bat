2ac8db1e6bc8de8b4ba6271f53751ce8a3b418bc
Steven De George SR <ehomecreations@gmail.com>
2026-01-24 12:01:44 +0200

    Changed to run new compiler

--- c1.bat
+++ c1.bat
@@ -1,4 +1,6 @@
-rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 hd.p8
-java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 test1.p8
+rem java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 test1.p8
+java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c-12.1-SNAPSHOT-all.jar -target cx16 test1.p8
+
+
 pause
 rem x16noCard1x.bat
--- src/c.bat
+++ src/c.bat
@@ -1,11 +1,11 @@
 echo COMPILED CX16 STREE
-rem java.exe -jar C:\8bitProgramming\prog8\prog8c-11.4.1-all.jar -target cx16 stree.p8
 del stree.prg
 del ..\stree.prg
-java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
+rem java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
+java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
 rem pause
 copy stree.prg ..\stree.prg
-del stree.asm
+del *.asm
 del stree.vice*.*
 rem x16noCard1x.bat
 
