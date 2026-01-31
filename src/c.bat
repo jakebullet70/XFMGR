79371eec4b08604f71bd8c9d1e10306f0567614c
Steven De George SR <ehomecreations@gmail.com>
2026-01-31 12:07:25 +0200

    Update for BASIC

--- src/c.bat
+++ src/c.bat
@@ -2,7 +2,7 @@ echo COMPILED CX16 STREE
 del stree.prg
 del ..\stree.prg
 rem java.exe -jar C:\8bitProgramming\prog8\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
-java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.p8
+java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c-12.1-SNAPSHOT-all.jar -target cx16 stree.pb
 rem pause
 copy stree.prg ..\stree.prg
 del *.asm
