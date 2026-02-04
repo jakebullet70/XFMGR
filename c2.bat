76225c78112bdb64cbc4fed8162c4096281a0a14
Steven De George SR <ehomecreations@gmail.com>
2026-02-04 19:35:06 +0200

    Updated BAT files for compile

--- c2.bat
+++ c2.bat
@@ -1,3 +1,3 @@
-java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c-12.1-SNAPSHOT-all.jar -target cx16 test2.p8
+java.exe -jar C:\dev\CmdrX16\dos_tools\XFMGR\prog8c.jar -target cx16 test2.p8
 pause
 rem x16noCard1x.bat
